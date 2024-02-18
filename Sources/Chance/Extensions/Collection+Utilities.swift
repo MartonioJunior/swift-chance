28a2891198352a61f8a8aabe6942e51075acb0a5
Martônio Júnior <martoniojunior@hotmail.com>
2024-02-18 13:25:17 -0300

    feat(Collection): Added element and index generators

---
+++ Sources/Chance/Extensions/Collection+Utilities.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Martônio Júnior on 18/02/24.
+//
+
+import Foundation
+import Gen
+
+public extension Collection {
+    var elementGenerator: Gen<Element> {
+        Gen.element(of: self).compactMap({ $0 })
+    }
+    
+    var indexGenerator: Gen<Index> {
+        Gen.element(of: indices).compactMap({ $0 })
+    }
+}
