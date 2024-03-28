de4c0ef2fc3231f7ec631d5b680098cf52ffb0f3
Martônio Júnior <martoniojunior@hotmail.com>
2024-03-28 11:30:49 -0300

    chore: Added unit tests for the Seed-based RNG system

---
+++ Tests/ChanceTests/Seed/Seed+Tests.swift
@@ -0,0 +1,54 @@
+//
+//  Seed+Tests.swift
+//  
+//
+//  Created by Martônio Júnior on 21/03/24.
+//
+
+import XCTest
+@testable import Chance
+
+final class Seed_Tests: XCTestCase {
+    // MARK: Test Cases
+    func test_rawPosition_transformsIndexArrayIntoSingleValue() {
+        let seed = Seed(value: 24, position: [4,0,0,2,8,9,2,2])
+        
+        XCTAssertEqual(seed.rawPosition, 1800936799)
+    }
+    
+    func test_init_createsNewSeedWithIndex()
+    {
+        let seed = Seed(value: 13, position: [2,3,1])
+        
+        XCTAssertEqual(seed.value, 13)
+        XCTAssertEqual(seed.position, [2,3,1])
+    }
+    
+    func test_seed1D_initializesOneDimensionalSeed() {
+        let seed: Seed = .seed1D(seed: 12)
+        
+        XCTAssertEqual(seed.value, 12)
+        XCTAssertEqual(seed.position, [0])
+    }
+
+    func test_seed2D_initializesTwoDimensionalSeed() {
+        let seed: Seed = .seed2D(seed: 4)
+        
+        XCTAssertEqual(seed.value, 4)
+        XCTAssertEqual(seed.position, [0,0])
+    }
+
+    func test_seed3D_initializesThreeDimensionalSeed() {
+        let seed: Seed = .seed3D(seed: 98)
+        
+        XCTAssertEqual(seed.value, 98)
+        XCTAssertEqual(seed.position, [0,0,0])
+    }
+
+    func test_seed4D_initializesFourDimensionalSeed() {
+        let seed: Seed = .seed4D(seed: 9)
+        
+        XCTAssertEqual(seed.value, 9)
+        XCTAssertEqual(seed.position, [0,0,0,0])
+    }
+}
---
+++ Tests/ChanceTests/Seed/SeedRandomNumberGenerator+Tests.swift
@@ -0,0 +1,68 @@
+//
+//  SeedRandomNumberGenerator+Tests.swift
+//  
+//
+//  Created by Martônio Júnior on 21/03/24.
+//
+
+import XCTest
+import Gen
+@testable import Chance
+
+final class SeedRandomNumberGenerator_Tests: XCTestCase {
+    // MARK: Test Utilities
+    func scoreRNG(_ generator: Gen<UInt64>) -> Double {
+        let numberOfAttempts = 1000
+        
+        let result = generator.set(ofAtMost: .always(1000)).run()
+        
+        return Double(result.count) / Double(numberOfAttempts)
+    }
+    // MARK: Test Cases
+    func test_init_createsGeneratorBasedOnFormulaAndSeed() {
+        let seed: Seed = .seed1D(seed: 24)
+        let rng = SeedRandomNumberGenerator(seed)
+        
+        XCTAssertEqual(rng.seed.value, seed.value)
+        XCTAssertEqual(rng.seed.position, seed.position)
+        XCTAssertEqual(rng.formula(2,3), SeedRNG.squirrelFive(seed: 2, position: 3))
+    }
+    
+    func test_advanceState_changesSeedState() {
+        var rng = SeedRandomNumberGenerator(.seed3D(seed: 67))
+        rng.advanceState()
+        
+        XCTAssertEqual(rng.seed.position, [1,0,0])
+    }
+    
+    func test_peek_revealsNextValue() {
+        let formula: SeedRandomNumberGenerator.Formula = { seed,_ in 9 }
+        let rng = SeedRandomNumberGenerator(.seed1D(seed: 67), formula: formula)
+        
+        XCTAssertEqual(rng.peek(), 9)
+    }
+    // MARK: SeedRandomNumberGenerator: RandomNumberGenerator
+    func test_next_returnsNextValueAdvancingState() {
+        let formula: SeedRandomNumberGenerator.Formula = { seed,_ in 9 }
+        var rng = SeedRandomNumberGenerator(.seed2D(seed: 67), formula: formula)
+        let result = rng.next()
+        
+        XCTAssertEqual(result, 9)
+        XCTAssertEqual(rng.seed.position, [1,0])
+    }
+    
+    // MARK: Algorithms
+    func test_squirrelThree_generatesUniqueValues() {
+        var rng = SeedRandomNumberGenerator(.seed3D(seed: 67), formula: SeedRNG.squirrelThree)
+        let generator: Gen<UInt64> = .f { rng.next() }
+        let score = scoreRNG(generator)
+        XCTAssertGreaterThanOrEqual(score, 0.8)
+    }
+    
+    func test_squirrelFive_generatesUniqueValues() {
+        var rng = SeedRandomNumberGenerator(.seed3D(seed: 67), formula: SeedRNG.squirrelFive)
+        let generator: Gen<UInt64> = .f { rng.next() }
+        let score = scoreRNG(generator)
+        XCTAssertGreaterThanOrEqual(score, 0.8)
+    }
+}
