ec857d75b5bef2eb83f5c83c3dba1dd19de5a6ee
Martônio Júnior <martoniojunior@hotmail.com>
2024-12-09 21:38:40 -0300

    change(BinaryFloatingPoint): Updated Test Cases to Swift Testing

--- Tests/SwiftChanceTests/swift-gen/BinaryFloatingPoint+Tests.swift
+++ Tests/SwiftChanceTests/swift-gen/BinaryFloatingPoint+Tests.swift
@@ -5,41 +5,24 @@
 //  Created by Martônio Júnior on 27/11/23.
 //
 
-import XCTest
-import Gen
+import Testing
+@preconcurrency import Gen
 @testable import SwiftChance
 
-final class BinaryFloatingPoint_Tests: XCTestCase {
-    typealias RNG = SystemRandomNumberGenerator
-    // MARK: Utilities
-    public func testGenerator(range: ClosedRange<Double> = -1_000_000_000...1_000_000_000) -> (Gen<Double>) -> Bool {
-        {
-            $0.array(of: .always(100)).run().allSatisfy((range).contains)
-        }
+struct BinaryFloatingPointTests {
+    @Test("Returns value inside valid Range", arguments: [
+        (Gen<Double>.binaryFloatingPoint, -1_000_000_000...1_000_000_000),
+        (Gen<Double>.normalBinaryFloatingPoint, 0...1),
+        (Gen<Double>.signedBinaryFloatingPoint, -1...1)
+    ])
+    func evaluateGenerators(gen: Gen<Double>, range: ClosedRange<Double>) async throws {
+        #expect(gen.array(of: .always(100)).run().allSatisfy(range.contains))
     }
     
-    // MARK: Test Cases
-    func test_binaryFloatingPoint_returnsValueInsideValidRange() {
-        let generator: Gen<Double> = .binaryFloatingPoint
-        
-        XCTAssertTrue(testGenerator()(generator))
-    }
-    
-    func test_normalBinaryFloatingPoint_returnsValueBetweenZeroAndOne() {
-        let generator: Gen<Double> = .normalBinaryFloatingPoint
-
-        XCTAssertTrue(testGenerator(range: 0...1)(generator))
-    }
-    
-    func test_signedBinaryFloatingPoint_returnsValueBetweenPositiveAndMinusOne() {
-        let generator: Gen<Double> = .signedBinaryFloatingPoint
-
-        XCTAssertTrue(testGenerator(range: -1...1)(generator))
-    }
-    
-    func test_random_returnsNewValue() {
-        let value: Gen<Double> = .f { .random() }
-        
-        XCTAssertTrue(testGenerator()(value))
+    @Test("Returns valid Binary Floating Point value")
+    func random() async throws {
+        let gen: Gen<Double> = .f(Double.random)
+        let range: ClosedRange<Double> = -1_000_000_000...1_000_000_000
+        #expect(gen.array(of: .always(100)).run().allSatisfy(range.contains))
     }
 }
