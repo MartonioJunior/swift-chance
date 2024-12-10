f30fe9b476555b82763cd1f3157829b2a096b7c6
Martônio Júnior <martoniojunior@hotmail.com>
2024-12-09 22:08:22 -0300

    change(Bool): Updated Test Cases to Swift Testing

--- Tests/SwiftChanceTests/swift-gen/Bool+Tests.swift
+++ Tests/SwiftChanceTests/swift-gen/Bool+Tests.swift
@@ -5,31 +5,19 @@
 //  Created by Martônio Júnior on 21/03/24.
 //
 
-import XCTest
-import Gen
+import Testing
+@preconcurrency import Gen
 @testable import SwiftChance
 
-final class Bool_Tests: XCTestCase {
-    func test_chance_returnsGeneratorWithOddsOfTrue()
-    {
-        let alwaysTrue: Gen<Bool> = .chance(1)
-        let alwaysFalse: Gen<Bool> = .chance(0)
-        
-        let checkCondition: (Bool) -> (Gen<Bool>) -> Bool = { result in
-            { generator in
-                generator.array(of: .always(100)).run().allSatisfy { element in
-                    element == result
-                }
-            }
-        }
-        
-        XCTAssertTrue(checkCondition(true)(alwaysTrue))
-        XCTAssertTrue(checkCondition(false)(alwaysFalse))
-        
-        let valueOverOdds: Gen<Bool> = .chance(0.5, generator: .f({ 0.6 }))
-        let valueUnderOdds: Gen<Bool> = .chance(0.5, generator: .f({ 0.4 }))
-        
-        XCTAssertTrue(checkCondition(true)(valueUnderOdds))
-        XCTAssertTrue(checkCondition(false)(valueOverOdds))
+struct BoolTests {
+    @Test("Returns Generator with odds of True", arguments: [
+        (1, Gen<Double>.binaryFloatingPoint, true),
+        (0, Gen<Double>.binaryFloatingPoint, false),
+        (0.5, Gen<Double>.always(0.6), false),
+        (0.5, Gen<Double>.always(0.4), true),
+    ])
+    func chance(odds: Double, gen: Gen<Double>, outcome: Bool) async throws {
+        let generator: Gen<Bool> = .chance(odds, generator: gen)
+        #expect(generator.run() == outcome)
     }
 }
